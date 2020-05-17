e484b2ef49ff96310608505a19786f7ab5bf4bf9
Luan Prestes <luan.prestes@tetris.co>
2020-05-17 20:28:02 -0300

    add route files

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import routes from './routes'
 
 class App {
   public express: express.Application
@@ -7,11 +8,16 @@ class App {
   public constructor () {
     this.express = express()
     this.middlewares()
+    this.routes()
   }
 
   private middlewares (): void {
     this.express.use(express.json())
     this.express.use(cors())
+  }
+
+  private routes (): void {
+    this.express.use(routes)
   }
 }
 
