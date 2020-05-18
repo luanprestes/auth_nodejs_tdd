9c7bab56a0c0711a30f5f5fe66a27f4cdaecd371
Luan Prestes <luan.prestes@tetris.co>
2020-05-17 21:58:03 -0300

    create file sample for variables envirement

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from './app'
 
-app.listen(3333, () => {
-  console.log('running port 3333')
+app.listen(process.env.NODE_PORT, () => {
+  console.log(`running port ${process.env.NODE_PORT}`)
 })
